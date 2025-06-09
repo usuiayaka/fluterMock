e4439f01cb77c86091676f6e2bac58c3d490e2d4
MSI\ayaka <usuiayaka22418123@gmail.com>
2025-06-09 15:31:49 +0900

    データをタイル表示に変更

--- lib/collectionPage.dart
+++ lib/collectionPage.dart
@@ -30,34 +30,35 @@ class _CollectionPageState extends State<CollectionPage> {
       body: Scrollbar(
         child: CustomScrollView(slivers: [
           SliverAppBar(
-              centerTitle: true,
-              floating: true,
-              title: Text(
-                "Your Collections",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w700),
-              ),
-              actions: [
-                TextButton(
-                    onPressed: () {
-                      // 登録用ページへ
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (_) => const Registrationpage(),
-                          ));
-                    },
-                    child: Text(
-                      '登録する',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ))
-              ]),
+            centerTitle: true,
+            floating: true,
+            title: Text(
+              "Your Collections",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    // 登録用ページへ
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => const Registrationpage(),
+                        ));
+                  },
+                  child: Text(
+                    '登録する',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ))
+            ],
+          ),
           FutureBuilder<List<Tea>>(
             future: _futureTeas,
             builder: (context, snapshot) {
@@ -74,11 +75,17 @@ class _CollectionPageState extends State<CollectionPage> {
               final teas = snapshot.data!;
               return SliverPadding(
                 padding: const EdgeInsets.all(8),
-                sliver: SliverList(
+                sliver: SliverGrid(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // ２列にする
+                    mainAxisSpacing: 8,
+                    crossAxisSpacing: 8,
+                    childAspectRatio: 1, // 正方形にする
+                  ),
                   delegate: SliverChildBuilderDelegate(
                     (context, index) {
                       final tea = teas[index];
-                      return TeaListTile(tea: tea);
+                      return TeaGridTile(tea: tea);
                     },
                     childCount: teas.length,
                   ),
@@ -108,32 +115,49 @@ class _CollectionPageState extends State<CollectionPage> {
   }
 }
 
-class TeaListTile extends StatelessWidget {
-  const TeaListTile({super.key, required this.tea});
+class TeaGridTile extends StatelessWidget {
+  const TeaGridTile({super.key, required this.tea});
 
   final Tea tea;
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.symmetric(vertical: 8),
+      clipBehavior: Clip.antiAlias, // 角丸＋画像切り取り
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
       child: InkWell(
         onTap: () => _showDetails(context),
-        child: Row(
+        child: Stack(
           children: [
-            Image.network(
-              tea.image,
-              width: 100,
-              height: 100,
-              fit: BoxFit.cover,
-              errorBuilder: (context, error, StackTrace) => Icon(Icons.image),
+            Positioned.fill(
+              child: Image.network(
+                tea.image,
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, StackTrace) =>
+                    Icon(Icons.image, size: 50),
+              ),
             ),
-            const SizedBox(width: 16),
-            Expanded(
+            Positioned(
+              bottom: 8,
+              left: 8,
+              right: 8,
+              child: Container(
+                color: Colors.black.withOpacity(0.4), // 半透明の背景
+                padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
                 child: Text(
-              tea.name,
-              style: Theme.of(context).textTheme.titleLarge,
-            ))
+                  tea.name,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -142,52 +166,47 @@ class TeaListTile extends StatelessWidget {
 
   void _showDetails(BuildContext context) {
     showGeneralDialog(
-        context: context,
-        barrierLabel: "tea_detail",
-        barrierDismissible: true,
-        pageBuilder: (
-          _,
-          __,
-          ___,
-        ) =>
-            Container(),
-        transitionBuilder: (ctx, a1, a2, child) {
-          final scale = Curves.easeInOut.transform(a1.value);
-          return Transform.scale(
-            scale: scale,
-            child: Dialog(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16)),
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Image.network(
-                      tea.image,
-                      width: 100,
-                      height: 100,
-                      fit: BoxFit.cover,
-                      errorBuilder: (context, error, StackTrace) =>
-                          Icon(Icons.image),
-                    ),
-                    const SizedBox(height: 12),
-                    Text(
-                      tea.description,
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                    SizedBox(height: 10),
-                    Text('味タイプ:${tea.tasteType}',
-                        style: Theme.of(context).textTheme.bodyMedium),
-                    Text('香り:${tea.aroma}',
-                        style: Theme.of(context).textTheme.bodyMedium),
-                    Text('色:${tea.color}',
-                        style: Theme.of(context).textTheme.bodyMedium),
-                  ],
-                ),
+      context: context,
+      barrierLabel: "tea_detail",
+      barrierDismissible: true,
+      pageBuilder: (_, __, ___) => Container(),
+      transitionBuilder: (ctx, a1, a2, child) {
+        final scale = Curves.easeInOut.transform(a1.value);
+        return Transform.scale(
+          scale: scale,
+          child: Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.network(
+                    tea.image,
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, StackTrace) =>
+                        Icon(Icons.image),
+                  ),
+                  const SizedBox(height: 12),
+                  Text(tea.description,
+                      style: Theme.of(context).textTheme.bodyMedium),
+                  SizedBox(height: 10),
+                  Text('味タイプ:${tea.tasteType}',
+                      style: Theme.of(context).textTheme.bodyMedium),
+                  Text('香り:${tea.aroma}',
+                      style: Theme.of(context).textTheme.bodyMedium),
+                  Text('色:${tea.color}',
+                      style: Theme.of(context).textTheme.bodyMedium),
+                ],
               ),
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 }
